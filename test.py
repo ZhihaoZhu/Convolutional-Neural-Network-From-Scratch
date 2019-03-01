56e36d392278448d5ecf60d051647a18577414ca
Steve Zhu <704242527zzh@gmail.com>
2019-03-01 01:00:27 -0500

    new

--- test.py
+++ test.py
@@ -1,16 +1,6 @@
-import numpy as np
-from nn import *
-import matplotlib.pyplot as plt
+import pickle
 
-W1 = np.load("./saved_model2/W1_30.npy")
-W1 = W1.T
-
-W1 = W1.reshape(100,28,56)
-fig=plt.figure()
-for i in range(1, 101):
-    img = W1[i-1,:,:]
-    fig.add_subplot(10, 10, i)
-    plt.imshow(img)
-    plt.xticks([])
-    plt.yticks([])
-plt.show()
+with open("../data/sampledCIFAR10" , "rb") as f :
+    data = pickle.load(f)
+    train, val, test = data["train"], data["val"], data["test"]
+    print("loaded success")
